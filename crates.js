9d6a19504ddf8f575cc0859cd62da399de301350
huhlig <huhlig@gmail.com>
2022-02-01 17:46:00 +0000

    Deploying to gh-pages from @ RoguishStudios/dicekit@be330c9922806d88c0d3fd1ce80228d72c7ae9a7 🚀

--- crates.js
+++ crates.js
@@ -1,1 +1,1 @@
-window.ALL_CRATES = ["cfg_if","getrandom","libc","ppv_lite86","rand","rand_chacha","rand_core","rogue_dicekit"];
+window.ALL_CRATES = ["cfg_if","dicekit","getrandom","libc","ppv_lite86","rand","rand_chacha","rand_core"];
